78075f8c01ba90e45b35d5c031f16dd37b81fdd5
Zidane Innis <zidaneinnis@gmail.com>
2022-08-12 16:53:51 -0500

    Reducers and Actions in Redux

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("From index.js");
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+const initialState = {
+  numOfCakes: 10,
+  anotherProperty: 0,
+};
+
+// (previousState, action)=>newState
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
