a3737f833a62d905353ca05619336c76ef16ab41
Zidane Innis <zidaneinnis@gmail.com>
2022-08-13 08:13:23 -0500

    Restocking cakes to demonstrate Redux pattern

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: 1,
   };
 }
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+
 const initialState = {
   numOfCakes: 10,
   anotherProperty: 0,
@@ -25,6 +34,12 @@ const reducer = (state = initialState, action) => {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+
     default:
       return state;
   }
@@ -40,6 +55,6 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
-store.dispatch(orderCake());
+store.dispatch(restockCake(payload));
 
 unsubscribe();
